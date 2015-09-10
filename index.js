03a20e6ddf64f53b674437a85aa8f882db77d538
oscarotero <oom@oscarotero.com>
2015-09-10 02:30:33 +0200

    space indentation

--- index.js
+++ index.js
@@ -2,46 +2,46 @@
 
 module.exports = function (tasks) {
 
-	var allCustomMedia = {};
+    var allCustomMedia = {};
 
-	//Reset customMedia variable
-	tasks.addTask({
-		position: 'before',
-		fn: function (root) {
-			allCustomMedia = {};
-		}
-	});
+    //Reset customMedia variable
+    tasks.addTask({
+        position: 'before',
+        fn: function (root) {
+            allCustomMedia = {};
+        }
+    });
 
-	//Save all custom-media
-	tasks.addTask({
-		filter: {
-			type: 'AtRule',
-			name: 'custom-media'
-		},
-		fn: function (customMedia) {
-			allCustomMedia[customMedia.get('ExtensionName').name] = customMedia.get('MediaQuery');
-			customMedia.detach();
-		}
-	});
+    //Save all custom-media
+    tasks.addTask({
+        filter: {
+            type: 'AtRule',
+            name: 'custom-media'
+        },
+        fn: function (customMedia) {
+            allCustomMedia[customMedia.get('ExtensionName').name] = customMedia.get('MediaQuery');
+            customMedia.detach();
+        }
+    });
 
-	//Replace the custom-media
-	tasks.addTask({
-		filter: {
-			type: 'AtRule',
-			name: 'media'
-		},
-		fn: function (media) {
-			media.walk('ExtensionName', function (extension) {
-				var mediaquery = allCustomMedia[extension.name];
+    //Replace the custom-media
+    tasks.addTask({
+        filter: {
+            type: 'AtRule',
+            name: 'media'
+        },
+        fn: function (media) {
+            media.walk('ExtensionName', function (extension) {
+                var mediaquery = allCustomMedia[extension.name];
 
-				if (mediaquery) {
-					let expression = extension.getParent('ConditionalExpression');
+                if (mediaquery) {
+                    let expression = extension.getParent('ConditionalExpression');
 
-					mediaquery.forEach(child => expression.before(child.clone()));
-					expression.detach();
-					//extension.getParent('ConditionalExpression').replaceWith(mediaqueries.clone());
-				}
-			});
-		}
-	});
+                    mediaquery.forEach(child => expression.before(child.clone()));
+                    expression.detach();
+                    //extension.getParent('ConditionalExpression').replaceWith(mediaqueries.clone());
+                }
+            });
+        }
+    });
 };
